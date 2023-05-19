9e36626012f4e31298005748492add035bc343f7
Caner Emec <caner.emec@gmail.com>
2023-05-19 13:30:23 +0300

    tes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,1 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  ignores: [message => message.includes('WIP')],
-};
+module.exports = {extends: ['@commitlint/config-conventional']};
